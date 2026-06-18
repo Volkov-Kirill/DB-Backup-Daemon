d1c83733208cd9b320294680b87992a37c9f5f21
nikitaToner <x1tle@yandex.ru>
2026-06-18 12:12:05 +0700

    База для проекта
    Создана система логов
    Созданы папки для более удобной работы над проектом
    Начал базовые функции

---
+++ project/DumpSystem/ArchiverManager/ArchiverManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Serilog;
+using project.DumpSystem.DumpManager;
+
+namespace project.DumpSystem.ArchiverManager
+{
+    
+    public class ArchiverManager
+    {
+        Addition addition = new Addition();
+        DumperManager Dumper = new DumperManager();
+
+        public void Init(string url)
+        {
+            if (addition.CheckUrl(url))
+            {
+                Log.Information("Начинаем дамп");
+                Dumper.IntallDumper();
+            }
+        }
+
+        
+
+
+    }
+}
