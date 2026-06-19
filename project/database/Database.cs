e985987af3f84916aa0c9f8cdcb52c09e69648be
nikitaToner <x1tle@yandex.ru>
2026-06-19 12:37:58 +0300

    Сделал базу данных
    Сделал базу данных для сохранение пользователей и их бэкапов

---
+++ project/database/Database.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Serilog;
+using Microsoft.Data.Sqlite;
+
+namespace project.Database
+{
+    public class Database
+    {
+
+        public void installDatabase(string databaseName)
+        {
+            {
+             
+                using (var connection = new SqliteConnection(databaseName))
+                {
+                    connection.Open();
+
+                    string createUsersTable = @"
+                CREATE TABLE IF NOT EXISTS Users (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Name TEXT NOT NULL,
+                    Password TEXT NOT NULL
+                );
+            ";
+
+                    string createBackupsTable = @"
+                CREATE TABLE IF NOT EXISTS Backups (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    UserId INTEGER NOT NULL,
+                    BackupName TEXT,
+                    Location TEXT,
+                    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (UserId) REFERENCES Users(Id) ON DELETE CASCADE
+                );
+            ";
+
+                    using (var command = new SqliteCommand(createUsersTable, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    using (var command = new SqliteCommand(createBackupsTable, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    Log.Information("База данных создана");
+                }
+            }
+        }
+    }
+}
