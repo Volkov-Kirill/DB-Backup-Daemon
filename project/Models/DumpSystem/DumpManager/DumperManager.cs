9bac815971ad49011c82ca15ca5ca834b903bc1f
nikitaToner <x1tle@yandex.ru>
2026-06-22 15:15:05 +0700

    Обновление dumpManager, теперь возможно сделать дамп

--- project/Models/DumpSystem/DumpManager/DumperManager.cs
+++ project/Models/DumpSystem/DumpManager/DumperManager.cs
@@ -1,14 +1,44 @@
+using Microsoft.Win32;
+using Serilog;
+using SharpCompress.Archives.Zip;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
+using System.Windows;
+using Path = System.IO.Path;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+using ZipArchive = System.IO.Compression.ZipArchive;
 
 namespace project.DumpSystem.DumpManager
 {
     public class DumperManager
     {
-        public void IntallDumper() { }
+        public void IntallDumper(string zipPath,string folderPath) 
+        {
+            try
+            {
+                using (ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Create))
+                {
+                    var sqlFiles = Directory.GetFiles(folderPath, "*.sql");
+                    foreach (var file in sqlFiles)
+                    {
+                        zip.CreateEntryFromFile(file, Path.GetFileName(file));
+                    }
+                    Log.Information("Дамп готов");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Information(ex.Message);
+            }
+
+
+        }
+       
     }
 }
